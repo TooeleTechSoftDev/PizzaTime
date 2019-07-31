a0b934937aa4e098c9c63fff925e77f9f2dc1d50
Kerns Phoegon (PC Laptops) <k.phoegon@gmail.com>
2019-07-31 13:40:32 -0600

    Update before pull

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-const express = require('express');
-const app = express();
-var mongo = require('mongodb');
+const db = require('mongodb')
+const dblink = "mongodb://localhost:27017"
+const MongoClient = db.MongoClient
+const assert = require('assert')
+const express = require('express')
+const app = express()
+const mongoClient = new MongoClient(dblink, { useNewUrlParser: true })
+const DBname = 'PizzaTime'
+
+mongoClient.connect(function (err) {
+    assert.equal(null, err)
+    console.log("Connected to Mongo")
+    app.listen("3000", () => {
+        console.log("Server started on 3000")
+    })
+})
+
+function look(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).find(entr, { projection: { _id: 0 } }).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+}
+function entry(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).insertOne(entr, function (err, res) {
+        if (err) throw err
+        console.log("1 document inserted")
+    })
+}
+function remove(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).deleteOne(entr, function (err, obj) {
+        if (err) throw err
+        console.log("1 document deleted")
+    })
+}
+
+    /* data = database name
+*      - NoSpaces
+* collect = collection name
+*      - NoSpaces
+* entry = Entry added, as object
+*         - Example - { name: "Company Inc", address: "Highway 37" }
+* */
 
 app.use('/public',express.static('public'))
 
@@ -11,11 +54,21 @@ let jsonObj = { "name": "supreme",
                 "size": 12,
                 "crust": "deep",
                 "imgURL":  "http://www.sugardale.com/sites/default/files/stuffed%20crust%20pizza.jpg"
-            }
-                
-
+}
+app.get('/account/lookup/:name', (req, res) => {
+    let lname = JSON.stringify(req.params.name)
+    let test = '{ "name": ' + lname + ' }'
+    console.log('test is ' + test)
+    let intest = JSON.parse(test)
+    let output = look("Pizza-dude", "People", intest)
+    console.log('before')
+    console.log(output)
+    console.log('after')
+})
+/*
+var test = { name: "Bobby" }
+look("Pizza-dude", "People", test)
+*/
 app.get('/deals', (req, res) => res.send(JSON.stringify(jsonObj)))
 
-app.listen(3000, 
-    () => console.log("Listening on port 3000"))
 
--- src/db.js
+++ src/db.js
@@ -1,48 +1,50 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+const db = require('mongodb')
+const dblink = "mongodb://localhost:27017"
+const MongoClient = db.MongoClient
+const assert = require('assert')
 
-/* entr, requirement : at end [ field: "field data" ] examples, : Included, Quotes included */
-/* data,  the database [ "Name" ]  Quotes included*/
-/* collect, the collection database name [ "Name" ] quotes included */
+const mongoClient = new MongoClient(dblink, { useNewUrlParser: true })
+const DBname = 'PizzaTime'
 
-function look(data, collect, entr, adr) {
-    MongoClient.connect(adr, function (err, db) {
+mongoClient.connect(function (err) {
+    assert.equal(null, err)
+    console.log("Connected to Mongo")
+    app.listen("3000", () => {
+        console.log("Server started on 3000")
+    })
+})
+
+function look(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).find(entr, { projection: { _id: 0 } }).toArray(function (err, result) {
         if (err) throw err;
-        var dbo = db.db(data);
-        dbo.collection(collect).find({ entr }, { projection: { _id: 0 } }).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-        });
+        console.log(result);
     });
 }
-/* entering - example [name: "Company Inc", address: "Highway 37"]  : included, Quotes included */
-
-function entry(data, collect, entering, adr) {
-    MongoClient.connect(adr, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(data);
-        dbo.collection(collect).insertOne(entering, function (err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-        });
-    });
+function entry(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).insertOne(entr, function (err, res) {
+        if (err) throw err
+        console.log("1 document inserted")
+    })
+}
+function remove(data, collect, entr) {
+    const dbo = mongoClient.db(data)
+    dbo.collection(collect).deleteOne(entr, function (err, obj) {
+        if (err) throw err
+        console.log("1 document deleted")
+    })
+   
 }
 
-function remove(data, collect, entr, adr) {
-    MongoClient.connect(adr, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(data);
-        var myquery = { entr };
-        dbo.collection(collect).deleteOne(myquery, function (err, obj) {
-            if (err) throw err;
-            console.log("1 document deleted");
-            db.close();
-        });
-    }); 
-}
+    /* data = database name
+*      - NoSpaces
+* collect = collection name
+*      - NoSpaces
+* entry = Entry added, as object
+*         - Example - { name: "Company Inc", address: "Highway 37" }
+* */
+var test = { name: "Bobby" }
+look("Pizza-dude", "People", test)
 
 
-var ent = { name: "Bobby" }
-entry("Example", "People", ent, url);
