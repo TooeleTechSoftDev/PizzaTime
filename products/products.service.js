34fedea6ce03e9d9ae51a790ef8b95b47575306c
Jon Stout <jon-gh@abovecs.com>
2019-09-24 15:48:44 -0600

    add/fix Products functions

--- products/products.service.js
+++ products/products.service.js
@@ -1,20 +1,39 @@
 const dbclient = require('../helpers/db')
+const { checkInput } = require('../helpers/schemas')
 
-var collection 
+
+let collection 
 (async () => { collection = await dbclient.collections() })()
 
 module.exports = {
     getProdById,
-    getProdsByCategory
+    getProdByCategory,
+    newProduct,
+    changeProduct
 }
 
 async function getProdById(id) {
-    console.log("Looking for productID: ", id)
-    return await collection.products.findOne({ "productID": id })    
+    // console.log("Looking for productId: ", id)
+    return await collection.Products.findOne({ "productId": id })    
 }
 
-async function getProdsByCategory(category) {
-    console.log("Looking for product category: ", category)
-    return await collection.products.find({ "category": category }).toArray() 
+async function getProdByCategory(category) {
+    // console.log("Looking for product category: ", category)
+    return await collection.Products.find({ "orderType": category }).toArray() 
 }
 
+async function newProduct(productData) {
+    if (productData.productId)  throw('productId should NOT exist on received data')
+    productData.productId = Math.floor(Math.random() * 10000) + 10000;
+    // *** verify Id doesn't already exist in database?
+
+    err = checkInput(productData, 'product')
+    if (err)  throw(err)
+
+    return await collection.Products.insertOne(productData)
+}
+
+async function changeProduct(prodId, productData) {
+    // *** Todo: sanitize the data and do security checks here.
+    return await collection.Products.updateOne( { "productId": prodId }, { $set: productData })
+}
